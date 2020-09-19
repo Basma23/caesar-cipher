560f8ac635b7f136ab4884b6da136948e4ccfaab
Basma Nizar Mohammed Eid <basmanizar@outlook.com>
2020-09-19 18:11:43 +0300

    Tests Added

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -1,3 +1,8 @@
+import nltk
+nltk.download('words')
+
+words_list= nltk.corpus.words.words()
+
 def encrypt(text, shift): 
     result = "" 
     for i in range(len(text)): 
@@ -18,6 +23,31 @@ def decrypt(text, shift):
             result += chr((ord(char) - shift - 97) % 26 + 97) 
     return result 
 
+def is_english(msg):
+    words = msg.split()
+  
+    word_count = 0
+
+    for word in words:
+        if word in words_list:
+            word_count += 1
+    if (word_count/len(words)) > 0.5:
+        return word_count
+    else: 
+        return 0
+
+def cipher_breaker(msg):
+    max = 0
+    max_english_sentence = ''
+    msg = msg.lower()
+    for key in range(26):
+        decrypted_msg = decrypt(msg ,(key))
+        count_words = is_english(decrypted_msg)
+        if count_words > max:
+            max_english_sentence = decrypted_msg
+    return max_english_sentence
+
+
 if __name__ == "__main__":
     print(encrypt('abc', 1))
     print(encrypt('abc', 10))
@@ -31,6 +61,7 @@ if __name__ == "__main__":
     print(decrypt('UpjlAyf', 7))
     print(encrypt('It was the best of times, it was the worst of times', 1))
     print(decrypt('Juoxbtouifocftuopgoujnftaojuoxbtouifoxpstuopgoujnft', 1))
+    print(cipher_breaker('Basma'))
     assert encrypt('abc', 1) == 'bcd'
     assert encrypt('abc', 10) == 'klm'
     assert encrypt('abc', 26) == 'abc'
--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -1,5 +1,17 @@
 from caesar_cipher import __version__
-
+from caesar_cipher.caesar_cipher import *
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_encrypt():
+    assert encrypt('abc', 1) == 'bcd'
+    assert encrypt('abc', 10) == 'klm'
+    assert encrypt('abc', 26) == 'abc'
+    assert encrypt('BASMA', 30) == 'FEWQE'
+
+def test_decrypt():
+    assert decrypt('bcd', 1) == 'abc'
+    assert decrypt('klm', 10) == 'abc'
+    assert decrypt('abc', 26) == 'abc'
+    assert decrypt('FEWQE', 30) == 'BASMA'
